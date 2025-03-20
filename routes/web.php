2203f8c9f6ce7e5c22deec24bcace65a47f409c8
DavidIncarbone <david.incarbone@gmail.com>
2025-03-20 17:41:56 +0100

    add compact() method

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControllerProva;
 
 
-Route::get('/home', [ControllerProva::class, "index"])->name("home");
-Route::get("/contacts", [ControllerProva::class, "contacts"])->name("contacts");
+Route::get("/home",function(){
+    $data = "Primi passi con Laravel";
+    return view("home", ["message" => $data]);
+})->name("home");
+
+Route::get("/contacts", function(){
+    $contacts ="I nostri contatti";
+    return view("contacts", compact("contacts"));
+})->name("contacts");
+
 Route::get("/about", [ControllerProva::class, "about"])->name("about");
